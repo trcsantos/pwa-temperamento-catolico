03c23b22703c3f3a9e6abd75507662593bda5db1
Thiago Ricardo <trcsantos82@gmail.com>
2026-06-11 19:40:17 -0300

    corrige import do plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-plugin'
+import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
